3d516d1185990642a3146b6f98dd80a1ab83d2db
Stefano Magistri <Stefano.Magistri@utmostworldwide.com>
2020-03-09 14:37:56 +0000

    Adding a Map

--- BethanysPieShopHRM.Server/Pages/EmployeeDetailBase.cs
+++ BethanysPieShopHRM.Server/Pages/EmployeeDetailBase.cs
@@ -1,28 +1,38 @@
-﻿using BethanysPieShopHRM.Server.Services;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BethanysPieShopHRM.ComponentsLibrary.Map;
+using BethanysPieShopHRM.Server.Services;
 using BethanysPieShopHRM.Shared;
 using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BethanysPieShopHRM.Server.Pages
 {
-    public class EmployeeDetailBase: ComponentBase
+    public class EmployeeDetailBase : ComponentBase
     {
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
+        [Inject]
+        public IJobCategoryDataService JobCategoryDataService { get; set; }
+
         [Parameter]
         public string EmployeeId { get; set; }
 
-        [Inject]
-        public IEmployeeDataService EmployeeDataService { get; set; }
+        public List<Marker> MapMarkers { get; set; } = new List<Marker>();
+
+        protected string JobCategory = string.Empty;
 
         public Employee Employee { get; set; } = new Employee();
 
         protected override async Task OnInitializedAsync()
-        { 
-            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));             
-        }
+        {
+            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
 
-        public IEnumerable<Employee> Employees { get; set; } 
+            MapMarkers = new List<Marker>
+            {
+                new Marker{Description = $"{Employee.FirstName} {Employee.LastName}",  ShowPopup = false, X = Employee.Longitude, Y = Employee.Latitude}
+            };
+            JobCategory = (await JobCategoryDataService.GetJobCategoryById(Employee.JobCategoryId)).JobCategoryName;
+        }
     }
 }
